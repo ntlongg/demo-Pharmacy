e0f63948ece958e6d081bea4e64d53d1d76a8c01
Ntlongg <ntlongg723@gmail.com>
2024-12-17 12:40:01 +0700

    123

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,25 @@
-const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const router = require('./routes');
-const path = require('path');
+const express = require('express')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+require('dotenv').config()
+const connectDB =  require ('./config/db')
+const router = require('./routes')
 
-const app = express();
-
+const app = express()
 app.use(cors({
     origin : process.env.FONTEND_URL,
     credentials : true
 }))
+app.use(express.json())
+app.use(cookieParser())
+app.use("/api", router) 
 
-dotenv.config();
-
-app.use(express.json());
-app.use(cookieParser());
-app.use('/api', router);
-
-const PORT = process.env.PORT || 8000;
-
+const PORT = 8000 || process.env.PORT
 
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
         console.log("connect to db")
         console.log("long dep trai")
-		console.log(`Server is running on port ${PORT}`);
+        console.log(`Server is running on port ${PORT}`);
     })
 })
